b18eb4a1f9027047a0cf8d939ae7e3e0f26c2079
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-08-01 14:07:49 -0300

    feat: adiciona Loader para execeutar a aplicação

---
+++ vinicius_reis_zimmermann-trabalho-java/src/main/java/br/edu/infnet/Loader.java
@@ -0,0 +1,10 @@
+package br.edu.infnet;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Loader implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
